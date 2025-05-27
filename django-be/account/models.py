544f058ab11fd363a627a56ded3be9536ba84c62
vvduth <ducthai060501@gmail.com>
2025-05-27 15:09:20 +0300

    Refactor user model to use name instead of username and improve email validation

--- django-be/account/models.py
+++ django-be/account/models.py
@@ -1,40 +1,37 @@
 from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.utils import timezone
 # Create your models here.
 
+
 class CustomUserManager(UserManager):
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, name, email, password, **extra_fields):
         if not email:
-            raise ValueError("The Email field must be set")
+            raise ValueError("You have not provided a valid e-mail address")
+
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
+    def create_user(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(name, email, password, **extra_fields)
 
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
+    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(name, email, password, **extra_fields)
 
 
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True, default='', null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, default='')
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
 
     is_active = models.BooleanField(default=True)
@@ -42,7 +39,7 @@ class User(AbstractUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     date_joined = models.DateTimeField(default=timezone.now)
-    last_login = models.DateTimeField(null=True, blank=True)
+    last_login = models.DateTimeField(blank=True, null=True)
 
     objects = CustomUserManager()
 
