548321f2a3c7c5953c81228ef5df3b1030e6c73d
vvduth <ducthai060501@gmail.com>
2025-05-27 15:47:47 +0300

    Refactor Feed and ProfileView components to use PostItem for rendering posts; update SearchView to enhance user interaction with dynamic links to profiles and improve search functionality.

--- vue-fe/src/router/index.ts
+++ vue-fe/src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: FeedView,
     },
     {
       path: '/signup',
--- vue-fe/src/views/SearchView.vue
+++ vue-fe/src/views/SearchView.vue
@@ -3,20 +3,24 @@
     <div class="main-center col-span-3 space-y-4">
       <div class="bg-white border border-gray-200 rounded-lg">
         <form @submit.prevent="submitSearch" class="p-4 flex space-x-4">
-          <input v-model="searchQuery" type="search" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you looking for?" />
+          <input v-model="searchQuery" type="search" class="p-4 w-full bg-gray-100 rounded-lg"
+            placeholder="What are you looking for?" />
 
-          <button href="#" class="inline-block py-4 px-6 bg-sky-600 text-white rounded-lg">Post</button>
+          <button href="#" class="inline-block py-4 px-6 bg-sky-600 text-white rounded-lg">Search</button>
         </form>
       </div>
 
       <!-- user result -->
       <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4">
-        <div class="p-4 text-center bg-gray-100 rounded-lg"
-        v-for="user in searchResultsUsers" :key="user.id">
+        <div class="p-4 text-center bg-gray-100 rounded-lg" v-for="user in searchResultsUsers" :key="user.id">
 
           <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
 
-          <p><strong>{{ user.name }}</strong></p>
+          <p><strong>
+              <RouterLink :to="{ name: 'profile', params: { 'id': user.id } }">
+                {{ user.name }}
+              </RouterLink>
+            </strong></p>
 
           <div class="mt-6 flex space-x-8 justify-around">
             <p class="text-xs text-gray-500">182 friends</p>
@@ -25,54 +29,12 @@
         </div>
       </div>
 
-      <div class="p-4 bg-white border border-gray-200 rounded-lg"
-        v-for="post in searchResultsPosts" :key="post.id">
-      
-        <div class="mb-6 flex items-center justify-between">
-          <div class="flex items-center space-x-6">
-            <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
+      <!-- post result feed -->
+      <div class="p-4 bg-white border border-gray-200 rounded-lg" 
+      v-for="post in searchResultsPosts" 
+      :key="post.id">
 
-            <p><strong>{{ post.created_by.name }}</strong></p>
-          </div>
-
-          <p class="text-gray-600">{{ post.created_at_formatted }}</p>
-        </div>
-
-        <p>
-          {{ post.body }}
-        </p>
-
-        <div class="my-6 flex justify-between">
-          <div class="flex space-x-6">
-            <div class="flex items-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-              </svg>
-
-              <span class="text-gray-500 text-xs">82 likes</span>
-            </div>
-
-            <div class="flex items-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
-              </svg>
-
-              <span class="text-gray-500 text-xs">3 comments</span>
-            </div>
-          </div>
-
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-            </svg>
-          </div>
-        </div>
+        <PostItem :post="post" />
       </div>
     </div>
 
@@ -86,34 +48,35 @@
 
 <script setup lang="ts">
 import PeopleYouMayKnow from '@/components/PeopleYouMayKnow.vue';
+import PostItem from '@/components/PostItem.vue';
 import Trends from '@/components/Trends.vue';
 import type { Post, SerachResponse, User } from '@/types';
 import axios from 'axios';
 import { ref } from 'vue';
 const searchQuery = ref('');
-const searchResultsUsers = ref<User[] >([]);
-const searchResultsPosts = ref<Post[] >([]);
+const searchResultsUsers = ref<User[]>([]);
+const searchResultsPosts = ref<Post[]>([]);
 
 const submitSearch = async (event: Event) => {
 
   event.preventDefault() // Optional since you're using .prevent
-  
+
   if (!searchQuery.value.trim()) {
     return // Don't search with empty query
   }
-  
+
   try {
-   
-    
+
+
     const res = await axios.post('/api/search/', {
       query: searchQuery.value
-    })  as SerachResponse
+    }) as SerachResponse
 
-    searchResultsUsers.value = res.data.users  
+    searchResultsUsers.value = res.data.users
     searchResultsPosts.value = res.data.posts
 
     console.log('Search results:', res.data)
-    
+
   } catch (error) {
     console.error('Search failed:', error)
   }
